e7eb986fa1abfee9d2e4353652c9d55ca52150a0
ivikaschauhan <rajputvikas8954@gmail.com>
2022-11-08 23:59:57 +0530

    as

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   // const [formValues, setFormValues]= useState(initialValues);
   return (
     <>
-    <h1>Bhosda</h1>
+    <h1>Hola</h1>
     <Switch>
       <Route exact path='/' component = {Login}  />
       <Route exact path='/register' component={Register} />
